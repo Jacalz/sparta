93fd64609675797abd604d1db9127b1badbbcc35
Jacob <Jacalz@users.noreply.github.com>
2019-12-03 15:16:34 +0100

    Updated xml functions (still unused)

--- src/xml.go
+++ src/xml.go
@@ -1,6 +1,12 @@
 package main
 
-import "encoding/xml"
+import (
+	"encoding/xml"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+)
 
 // Data has the xml data for the initial data tag and then incorporates Exercise.
 type Data struct {
@@ -18,4 +24,52 @@ type Exercise struct {
 	Reps     int     `xml:"reps"`
 	Sets     int     `xml:"sets"`
 	Comment  string  `xml:"comment"`
+}
+
+// XMLData contains the data from the xml file.
+var XMLData *Data
+
+// CheckDataFile does relevant checks around our data file.
+func CheckDataFile() {
+
+	configDir, _ := os.UserConfigDir()
+	file := filepath.Join(configDir, "sparta", "exercises.xml")
+
+	// Check if the user has a data file.
+	if _, err := os.Stat(file); err == nil { // The file does exist.
+		ReadDataFromXML(file)
+	} else if os.IsNotExist(err) { // The file doesn't exist, we should create it.
+		CreateFile(file)
+
+		// Now read the data
+	}
+}
+
+// ReadDataFromXML reads data from an xml file, couldn't be simpler.
+func ReadDataFromXML(filename string) {
+
+	// Open up the xml file that already exists.
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal("Could not find the file.", err)
+	}
+
+	// Make sure to close it also.
+	defer file.Close()
+
+	// Read the data from the opened file.
+	byteValue, _ := ioutil.ReadAll(file)
+
+	// Unmarshal the xml data in to our Data struct.
+	xml.Unmarshal(byteValue, &XMLData)
+}
+
+// CreateFile uses os.Create to make a file.
+func CreateFile(filename string) *os.File {
+	file, err := os.Create(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	return file
 }
