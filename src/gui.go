c6db9c84f5c37f12d0aee73d0155e38da8a5a7a5
Jacob <Jacalz@users.noreply.github.com>
2019-12-08 15:13:27 +0100

    Major gui updates

--- src/gui.go
+++ src/gui.go
@@ -10,6 +10,7 @@ import (
 	"fyne.io/fyne/theme"
 	"fyne.io/fyne/widget"
 	"sparta/file"
+	"strconv"
 )
 
 // PasswordKey contains the key taken from the username and password.
@@ -43,6 +44,9 @@ func Init(appName string) {
 			return
 		}
 
+		// Adapt the window to a good size.
+		window.Resize(fyne.NewSize(600, 400))
+
 		// Calculate the sha256 hash of the username and password.
 		PasswordKey = sha256.Sum256([]byte(userName.Text + userPassword.Text))
 
@@ -51,6 +55,45 @@ func Init(appName string) {
 
 		// The button for creating a new exercise.
 		newExercise := widget.NewButtonWithIcon("Add new exercise", theme.ContentAddIcon(), func() {
+			// Form information for the first row.
+			dateEntry := widget.NewEntry()
+			dateEntry.SetPlaceHolder("YYYY-MM-DD")
+
+			// Second row of form information.
+			clockEntry := widget.NewEntry()
+			clockEntry.SetPlaceHolder("HH:MM")
+
+			// Third row of the form.
+			activityEntry := widget.NewEntry()
+			activityEntry.SetPlaceHolder("Anything")
+
+			// Forth row of form data.
+			distanceEntry := widget.NewEntry()
+			distanceEntry.SetPlaceHolder("kilometers")
+			distance, _ := strconv.ParseFloat(distanceEntry.Text, 64)
+
+			// Fifth row in the form.
+			timeEntry := widget.NewEntry()
+			timeEntry.SetPlaceHolder("minutes")
+			time, _ := strconv.ParseFloat(timeEntry.Text, 64)
+
+			// Create the form for displaying.
+			form := &widget.Form{
+				OnSubmit: func() {
+					XMLData.Exercise = append(XMLData.Exercise, file.Exercise{Date: dateEntry.Text, Clock: clockEntry.Text, Activity: activityEntry.Text, Distance: distance, Time: time})
+					file.Write(XMLData)
+				},
+			}
+
+			// Append all the rows separately in to the form.
+			form.Append("Date", dateEntry)
+			form.Append("Clock", clockEntry)
+			form.Append("Activity", activityEntry)
+			form.Append("Distance", distanceEntry)
+			form.Append("Time", timeEntry)
+
+			// Show the popup dialog.
+			dialog.ShowCustom("Add activity", "Done", form, window)
 		})
 
 		// Create a label for displaing some info for the user. Default to showing nothing.
@@ -61,21 +104,15 @@ func Init(appName string) {
 
 		// Start up procedure if the data fiel is empty.
 		if !empty {
-			// Set a bigger window when we have content to display.
-			window.Resize(fyne.NewSize(400, 250))
-
 			// Add a new label for each exercise and so in a new goroutine to not block the current one.
 			go func() {
 				for i := range XMLData.Exercise {
 					vbox.Append(widget.NewLabel(fmt.Sprintf("At %s on %s, you trained %s. The distance was %v and the exercise lasted for %v minutes, resulting in an average speed of %v.",
 						XMLData.Exercise[i].Clock, XMLData.Exercise[i].Date, XMLData.Exercise[i].Activity, XMLData.Exercise[i].Distance,
-						XMLData.Exercise[i].Length, XMLData.Exercise[i].Distance/XMLData.Exercise[i].Length)))
+						XMLData.Exercise[i].Time, XMLData.Exercise[i].Distance/XMLData.Exercise[i].Time)))
 				}
 			}()
 		} else {
-			// Adapt the window size for empty content.
-			window.Resize(fyne.NewSize(400, 150))
-
 			// Inform about no exercies being avaliable.
 			label.SetText("No exercieses have been created yet.")
 		}
@@ -90,7 +127,7 @@ func Init(appName string) {
 	// Set the conatiner as what is being displayed.
 	window.SetContent(loginScreenContainer)
 
-	// Set a sane default for the window size.
+	// Set a sane default for the window size on login.
 	window.Resize(fyne.NewSize(400, 150))
 
 	// Show all of our set content and run the gui.
