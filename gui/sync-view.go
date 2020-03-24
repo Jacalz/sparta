90fe08de1f7cf294cc7ba1179536dce9bedfe736
Jacalz <jacob.alzen@gmail.com>
2020-03-24 11:21:35 +0100

    Improve sync gui by disabling share button when recieving

--- gui/sync-view.go
+++ gui/sync-view.go
@@ -1,7 +1,6 @@
 package gui
 
 import (
-	"fmt"
 	"sparta/gui/widgets"
 	"sparta/sync"
 
@@ -21,19 +20,16 @@ func (u *user) SyncView(window fyne.Window) fyne.CanvasObject {
 	// aboutGroup is a small group to display information about file sharing.
 	aboutGroup := widget.NewGroup("About Exercise Synchronization", widget.NewLabelWithStyle("The share support in Sparta enables the user to syncronize their exercises.\nThe file is served encrypted and will be automatically decrypted on a second device using the same login credentials.\nIt is as simple as starting the share on one device and then starting receiving on another device on the same network.\nThe receiving computer will then get all the new activities added locally.", fyne.TextAlignCenter, fyne.TextStyle{}))
 
+	// sendDataButton starts the network server and shares the file over the local internet.
+	startSendingDataButton := widget.NewButtonWithIcon("Start Syncing Exercises", theme.ViewRefreshIcon(), nil)
+
 	// recieveCode makes it possible to type in receive code.
 	recieveCodeEntry := widgets.NewAdvancedEntry("Receive Code", false)
 
 	// recieveDataButton starts looking for shared data on the local network.
-	recieveDataButton := widget.NewButtonWithIcon("Start Receiving Exercises", theme.MailComposeIcon(), func() {
-		go sync.Retrieve(&u.Data, u.ReorderExercises, u.FirstExercise, u.Errors, u.FinishedSync, &u.EncryptionKey, recieveCodeEntry.Text)
-	})
+	recieveDataButton := widget.NewButtonWithIcon("Start Receiving Exercises", theme.MailComposeIcon(), nil)
 
-	// Extend the recieveCodeEntry to add the option for receiving on pressing enter.
-	recieveCodeEntry.InitExtend(*recieveDataButton, widgets.MoveAction{})
-
-	// sendDataButton starts the network server and shares the file over the local internet.
-	startSendingDataButton := widget.NewButtonWithIcon("Start Syncing Exercises", theme.ViewRefreshIcon(), func() {
+	startSendingDataButton.OnTapped = func() {
 		if len(u.Data.Exercise) == 0 {
 			dialog.ShowInformation("No exercises to syncronize", "You need to add exercises to be able to sync between devices.", window)
 			return
@@ -44,29 +40,44 @@ func (u *user) SyncView(window fyne.Window) fyne.CanvasObject {
 
 		// Start the sharing.
 		go sync.StartSync(syncCodeChan, u.Errors, u.FinishedSync)
+	}
 
-	})
+	recieveDataButton.OnTapped = func() {
+		// Disable the button to make sure that users can't do anything bad.
+		startSendingDataButton.Disable()
+
+		// Start retrieving data.
+		go sync.Retrieve(&u.Data, u.ReorderExercises, u.FirstExercise, u.Errors, u.FinishedSync, &u.EncryptionKey, recieveCodeEntry.Text)
+	}
 
 	// shareGroup is a group containing all the options for sharing data.
 	shareGroup := widget.NewGroup("Syncronizing Data", startSendingDataButton)
 
-	// Listen in on the channels sent from receive and send.
+	// Listen in on the channels sent from receive and send and errors.
 	go func() {
 		for {
 			select {
 			case code := <-syncCodeChan:
 				recieveCodeEntry.SetText(code)
 			case <-u.FinishedSync:
+				dialog.ShowInformation("Synchronization successful", "The synchronization of exercises finsished successfully.", window)
+
+				// Clean up and make buttons usable again.
 				recieveCodeEntry.SetText("")
 				recieveDataButton.Enable()
-				dialog.ShowInformation("Synchronization successful", "The synchronization of exercises finsished successfully.", window)
+				startSendingDataButton.Enable()
 			case err := <-u.Errors:
-				fmt.Printf("Sync returned an error: %s", err)
 				dialog.ShowError(err, window)
+
+				// Clean up and make buttons usable again.
 				recieveDataButton.Enable()
+				startSendingDataButton.Enable()
 			}
 		}
 	}()
+
+	// Extend the recieveCodeEntry to add the option for receiving on pressing enter.
+	recieveCodeEntry.InitExtend(*recieveDataButton, widgets.MoveAction{})
 
 	// recieveGroup is a group containing all options related to receiving.
 	recieveGroup := widget.NewGroup("Receiving Data", fyne.NewContainerWithLayout(layout.NewGridLayout(2), recieveCodeEntry, recieveDataButton))
